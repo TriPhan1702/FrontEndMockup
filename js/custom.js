fc50ed98cf32f2d0fd49e130cb73b81be81dbac2
ComradeWang <tphan2883@gmail.com>
2020-02-07 22:23:06 +0700

    Add Cars Approval Page

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,18 @@ $(document).ready(function () {
   $(".button-collapse").sideNav({
     slim: true
   });
+
   // SideNav Scrollbar Initialization
   var sideNavScrollbar = document.querySelector('.custom-scrollbar');
   var ps = new PerfectScrollbar(sideNavScrollbar);
+
+  //Intitiate Datatable
+  $('#dtBasicExample').DataTable({
+    "searching": false // false to disable search (or any other option)
+  });
+  $('.dataTables_length').addClass('bs-select');
+  
+  $('.mdb-select').materialSelect();
 
   //Pie chart
   var ctxP = document.getElementById("labelChart").getContext('2d');
@@ -53,46 +62,46 @@ $(document).ready(function () {
     }
   });
 
-//bar
-var ctxB = document.getElementById("barChart").getContext('2d');
-var myBarChart = new Chart(ctxB, {
-type: 'bar',
-data: {
-labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-datasets: [{
-label: '# of Votes',
-data: [12, 19, 3, 5, 2, 3],
-backgroundColor: [
-'rgba(255, 99, 132, 0.2)',
-'rgba(54, 162, 235, 0.2)',
-'rgba(255, 206, 86, 0.2)',
-'rgba(75, 192, 192, 0.2)',
-'rgba(153, 102, 255, 0.2)',
-'rgba(255, 159, 64, 0.2)'
-],
-borderColor: [
-'rgba(255,99,132,1)',
-'rgba(54, 162, 235, 1)',
-'rgba(255, 206, 86, 1)',
-'rgba(75, 192, 192, 1)',
-'rgba(153, 102, 255, 1)',
-'rgba(255, 159, 64, 1)'
-],
-borderWidth: 1
-}]
-},
-options: {
-scales: {
-yAxes: [{
-ticks: {
-beginAtZero: true
-}
-}]
-}
-}
-});
+  //bar
+  var ctxB = document.getElementById("barChart").getContext('2d');
+  var myBarChart = new Chart(ctxB, {
+    type: 'bar',
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 
-var ctxSc = document.getElementById('scatterChart').getContext('2d');
+  var ctxSc = document.getElementById('scatterChart').getContext('2d');
 var scatterData = {
 datasets: [{
 borderColor: 'rgba(99,0,125, .2)',
@@ -235,6 +244,7 @@ display: true
 }
 }
 });
-  
-})
+});
+
+
 
